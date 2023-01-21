d4a31ac433e32d62f0d31221d9f6f87afa04e9d8
daeseungmoon <tmdeoans@snu,ac,jr>
2023-01-21 22:26:04 +0900

    notion column stored

--- packages/svelte-notion-page/src/lib/components/Column.svelte
+++ packages/svelte-notion-page/src/lib/components/Column.svelte
@@ -1,9 +1,36 @@
 <script lang="ts">
-	import type { ColumnProps } from "$lib/types";
-  export let props: ColumnProps
+	import type { ColumnProps } from '$lib/types';
+	export let props: ColumnProps;
 </script>
 
 <div class="notion-column">
-  <slot/>
+	<slot />
 </div>
-<div class="notion-column-spacer"/>
+<div class="notion-column-spacer" />
+
+<style>
+	.notion-column {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		gap: 2px;
+		padding-top: 16px;
+		padding-bottom: 16px;
+	}
+
+	/* notion spaceer */
+	.notion-column-spacer {
+		width: 46px;
+	}
+
+	.notion-column-spacer:last-child {
+		display: none;
+	}
+
+	@media (max-width: 640px) {
+		.notion-column {
+			padding-top: 0px;
+			padding-bottom: 0px;
+		}
+	}
+</style>
