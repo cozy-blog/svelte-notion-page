b8833df3ab730db1d1275d085b9f06bd2962dc19
daeseungmoon <tmdeoans@snu,ac,jr>
2023-01-21 17:56:49 +0900

    to-do refactored

--- packages/svelte-notion-page/src/lib/components/Todo.svelte
+++ packages/svelte-notion-page/src/lib/components/Todo.svelte
@@ -10,12 +10,34 @@
 	} = props;
 </script>
 
-<div class={`notion-block ${getColorCss(color)}`}>
-	<div aria-checked={checked} class:notion-to-do-checked={checked} class="notion-to-do">
-		<CheckBox {checked} />
-		<p class="notion-paragraph notion-to-do-content">
+<div class={`notion-block notion-to-do ${getColorCss(color)}`}>
+	<div aria-checked={checked} class:notion-to-do-checked={checked} class="notion-to-do-content">
+		<div class="notion-to-do-checkbox">
+			<CheckBox {checked} />
+		</div>
+		<p class="notion-to-do-text">
 			<RichText props={texts} />
 		</p>
 	</div>
 	<slot />
 </div>
+
+<style>
+	.notion-to-do-content {
+		display: flex;
+	}
+
+	.notion-to-do-checkbox {
+		display: inline-block;
+		padding: 8px 6px;
+	}
+
+	.notion-to-do-text {
+		padding: 3px 2px;
+	}
+
+	.notion-to-do-checked .notion-to-do-text {
+		text-decoration: line-through;
+		opacity: 0.6;
+	}
+</style>
