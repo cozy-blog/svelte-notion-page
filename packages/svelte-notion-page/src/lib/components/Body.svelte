118e67582c2e2103ffae1f35493cb14ee8d672ac
daeseungmoon <tmdeoans@snu.ac.kr>
2023-02-15 11:03:19 +0900

    little css modified

--- packages/svelte-notion-page/src/lib/components/Body.svelte
+++ packages/svelte-notion-page/src/lib/components/Body.svelte
@@ -1,15 +1,16 @@
 <article class="notion-body">
-  <slot/>
+	<slot />
 </article>
 
 <style>
-  .notion-body {
-    width: 100%;
-    max-width: var(--notion-max-width);
-    margin-left: auto;
-    margin-right: auto;
-    display: flex; 
-    flex-direction: column;
-    gap: 2px;
-  }
+	.notion-body {
+		width: 100%;
+		max-width: var(--notion-max-width);
+		margin-left: auto;
+		margin-right: auto;
+		display: flex;
+		padding: 0px 4px;
+		flex-direction: column;
+		gap: 2px;
+	}
 </style>
---
+++ packages/svelte-notion-page/src/stories/15_bookmark/notion-export.ts
@@ -0,0 +1,220 @@
+export const json = {
+	object: 'page',
+	id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b',
+	created_time: '2023-01-27T13:59:00.000Z',
+	last_edited_time: '2023-02-15T01:44:00.000Z',
+	created_by: {
+		object: 'user',
+		id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+	},
+	last_edited_by: {
+		object: 'user',
+		id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+	},
+	cover: {},
+	icon: null,
+	parent: {
+		type: 'database_id',
+		database_id: 'be65d799-9e98-4426-86a6-72072991e27b'
+	},
+	archived: false,
+	properties: {},
+	url: 'https://www.notion.so/Test-dffa6fc27f0946409ab0a6e26309140b',
+	blocks: [
+		{
+			object: 'block',
+			id: '93c1f00b-87dc-42f4-a700-00c724ed6a45',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-15T01:43:00.000Z',
+			last_edited_time: '2023-02-15T01:43:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'blue_background',
+				text: []
+			}
+		},
+		{
+			object: 'block',
+			id: '15410a7c-405d-4640-a7ae-7f0dcd57a4e8',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-15T01:43:00.000Z',
+			last_edited_time: '2023-02-15T01:44:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'bookmark',
+			bookmark: {
+				caption: [],
+				url: 'https://medium.com/@BlogAuthor/the-medium-author-guide-to-valentines-day-the-entertainment-earth-s-way-2423f4233268'
+			}
+		},
+		{
+			object: 'block',
+			id: 'c49702fa-a5ca-414a-9e6f-4d1bab816a47',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-15T01:43:00.000Z',
+			last_edited_time: '2023-02-15T01:44:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'blue_background',
+				text: []
+			}
+		},
+		{
+			object: 'block',
+			id: '615b1472-68e3-4f70-9b6c-c6fa0c7bcc0a',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-15T01:43:00.000Z',
+			last_edited_time: '2023-02-15T01:44:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: true,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'blue_background',
+				text: [
+					{
+						type: 'text',
+						text: {
+							content: 'asdf',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: 'asdf',
+						href: null
+					}
+				]
+			},
+			blocks: [
+				{
+					object: 'block',
+					id: '04721dfa-8a3e-4b1e-9b90-c4814566a661',
+					parent: {
+						type: 'block_id',
+						block_id: '615b1472-68e3-4f70-9b6c-c6fa0c7bcc0a'
+					},
+					created_time: '2023-02-15T01:44:00.000Z',
+					last_edited_time: '2023-02-15T01:44:00.000Z',
+					created_by: {
+						object: 'user',
+						id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+					},
+					last_edited_by: {
+						object: 'user',
+						id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+					},
+					has_children: false,
+					archived: false,
+					type: 'bookmark',
+					bookmark: {
+						caption: [],
+						url: 'https://medium.com/@BlogAuthor/the-medium-author-guide-to-valentines-day-the-entertainment-earth-s-way-2423f4233268'
+					}
+				}
+			]
+		},
+		{
+			object: 'block',
+			id: '74b839d3-8477-4c3f-ae99-2721dc28c578',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-15T01:44:00.000Z',
+			last_edited_time: '2023-02-15T01:44:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'bookmark',
+			bookmark: {
+				caption: [],
+				url: 'https://medium.com/@BlogAuthor/the-medium-author-guide-to-valentines-day-the-entertainment-earth-s-way-2423f4233268'
+			}
+		},
+		{
+			object: 'block',
+			id: 'c1e7d18b-2132-477b-aedd-727896f37bfc',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-15T01:44:00.000Z',
+			last_edited_time: '2023-02-15T01:44:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'default',
+				text: []
+			}
+		}
+	]
+};
