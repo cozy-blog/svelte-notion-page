6d936f342ff3cc0967ad159d0dd6fa56cdfbd879
daeseungmoon <tmdeoans@snu.ac.kr>
2023-02-16 10:07:05 +0900

    resolved cozy-blog/issue@35 notion color

---
+++ packages/svelte-notion-page/src/stories/0_pallette/notion-export.ts
@@ -0,0 +1,880 @@
+export const json = {
+	object: 'page',
+	id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b',
+	created_time: '2023-01-27T13:59:00.000Z',
+	last_edited_time: '2023-02-16T00:54:00.000Z',
+	created_by: {
+		object: 'user',
+		id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+	},
+	last_edited_by: {
+		object: 'user',
+		id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+	},
+	cover: {
+		type: 'external',
+		external: {
+			url: 'www.naver.com'
+		}
+	},
+	icon: null,
+	parent: {
+		type: 'database_id',
+		database_id: 'be65d799-9e98-4426-86a6-72072991e27b'
+	},
+	archived: false,
+	properties: {
+		HashTags: {
+			id: 'Hhkx',
+			type: 'multi_select',
+			multi_select: []
+		},
+		'생성 일시': {
+			id: 'J%7C%3BZ',
+			type: 'created_time',
+			created_time: '2023-01-27T13:59:00.000Z'
+		},
+		Slug: {
+			id: 'S%3A%7B%3E',
+			type: 'rich_text',
+			rich_rich_text: [
+				{
+					type: 'text',
+					text: {
+						content: 'asdf',
+						link: null
+					},
+					annotations: {
+						bold: false,
+						italic: false,
+						strikethrough: false,
+						underline: false,
+						code: false,
+						color: 'default'
+					},
+					plain_text: 'asdf',
+					href: null
+				}
+			]
+		},
+		텍스트: {
+			id: 'q%3CHh',
+			type: 'rich_text',
+			rich_rich_text: []
+		},
+		Description: {
+			id: 'qTV%3E',
+			type: 'rich_text',
+			rich_rich_text: []
+		},
+		Status: {
+			id: 'vu%7C%3B',
+			type: 'select',
+			select: {
+				id: '|QrX',
+				name: 'Publishable',
+				color: 'green'
+			}
+		},
+		Name: {
+			id: 'title',
+			type: 'title',
+			title: [
+				{
+					type: 'text',
+					text: {
+						content: 'Test',
+						link: null
+					},
+					annotations: {
+						bold: false,
+						italic: false,
+						strikethrough: false,
+						underline: false,
+						code: false,
+						color: 'default'
+					},
+					plain_text: 'Test',
+					href: null
+				}
+			]
+		}
+	},
+	url: 'https://www.notion.so/Test-dffa6fc27f0946409ab0a6e26309140b',
+	blocks: [
+		{
+			object: 'block',
+			id: '6b9f69d9-49ad-4cdc-9ca1-b489034a7359',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:53:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'gray_background',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: ' 1 회색',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: ' 1 회색',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: '5962a22e-9a9e-425c-b91c-597fb107c941',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:53:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'brown_background',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '2 갈색',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '2 갈색',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: 'dc92af10-d85c-4de6-8889-09d6028f58e7',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:53:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'orange_background',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '3 주황',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '3 주황',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: 'e52630d8-2b18-4bbe-bf08-88f32786ab1c',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:53:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'yellow_background',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '4 노랑',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '4 노랑',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: '44cf89ea-5a0b-41bd-a035-46227755bb7c',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:53:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'green_background',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '5 초록',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '5 초록',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: 'cc435c28-10e0-4bc7-9daa-f0cc2011c002',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:53:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'blue_background',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '6 파랑',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '6 파랑',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: 'd0690d91-b958-43d2-ae71-fda19d551629',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:53:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'purple_background',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '7 보라',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '7 보라',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: '6c0402a3-f628-48df-8674-936d75ef3670',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:53:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'pink_background',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '8 분홍',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '8 분홍',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: 'fa2dacdf-89b4-46b8-9381-d7b32d32e5cf',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:53:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'red_background',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '9 빨강',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '9 빨강',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: 'e3b0934a-f6a2-4a4e-9365-b5e77119775a',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:54:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'gray',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '10 회색',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '10 회색',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: '82b28a10-a452-40b6-8f11-1669b81f5f7a',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:54:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'brown',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '11 갈생',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '11 갈생',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: '0af8fa1a-edcd-4f97-8d2c-1075e3db042a',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:54:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'orange',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '12 주황',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '12 주황',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: 'f6bf21b2-6840-406c-90c1-5f8fd1b51559',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:54:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'yellow',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '13 노랑',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '13 노랑',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: '479b2a30-1105-466e-8a06-3f3faf6ddec0',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:54:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'green',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '14 초록',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '14 초록',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: '5a7e171e-2696-4ff8-8bec-8050e45a4c54',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:54:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'blue',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '15 파랑',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '15 파랑',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: '3917465d-e4b2-4fb5-b871-017a3f4a1d6b',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:54:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'purple',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '16 보라',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '16 보라',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: 'c4e88420-7fd1-4191-bc1b-3230033a04b6',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:54:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'pink',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '17 분홍',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '17 분홍',
+						href: null
+					}
+				]
+			}
+		},
+		{
+			object: 'block',
+			id: '279b8012-90eb-4205-b0ee-a2699e80f2b6',
+			parent: {
+				type: 'page_id',
+				page_id: 'dffa6fc2-7f09-4640-9ab0-a6e26309140b'
+			},
+			created_time: '2023-02-16T00:53:00.000Z',
+			last_edited_time: '2023-02-16T00:54:00.000Z',
+			created_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			last_edited_by: {
+				object: 'user',
+				id: '95fc0174-8fc6-4114-8e45-f67eacd99f07'
+			},
+			has_children: false,
+			archived: false,
+			type: 'paragraph',
+			paragraph: {
+				color: 'red',
+				rich_text: [
+					{
+						type: 'text',
+						text: {
+							content: '18 빨강',
+							link: null
+						},
+						annotations: {
+							bold: false,
+							italic: false,
+							strikethrough: false,
+							underline: false,
+							code: false,
+							color: 'default'
+						},
+						plain_text: '18 빨강',
+						href: null
+					}
+				]
+			}
+		}
+	]
+};
