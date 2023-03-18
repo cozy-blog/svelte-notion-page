367354dce9e70029d93b80092af49cdfb9660aef
daeseungmoon <tmdeoans@snu.ac.kr>
2023-03-18 23:24:15 +0900

    asdf

--- packages/svelte-notion-page/src/lib/types.ts
+++ packages/svelte-notion-page/src/lib/types.ts
@@ -1,6 +1,11 @@
-import type { GetPageResponse } from '@notionhq/client/build/src/api-endpoints';
-import type { ContentfulPage, Block as _Block } from '@cozy-blog/notion-client';
+import type {
+	GetPageResponse,
+	BlockObjectResponse,
+	PageObjectResponse
+} from '@notionhq/client/build/src/api-endpoints';
 
+export type _Block = BlockObjectResponse & { blocks: Block[] };
+export type ContentfulPage = PageObjectResponse & { blocks: Block[] };
 export type Page = GetPageResponse;
 export type Block = _Block;
 export type ContextedBlock = _Block & BlockContext & { blocks?: ContextedBlock[] };
