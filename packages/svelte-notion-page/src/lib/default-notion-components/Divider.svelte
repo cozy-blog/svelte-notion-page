56d41a9a68bcf43033d5a9803a6066d650979284
daeseungmoon <tmdeoans@snu.ac.kr>
2023-05-02 14:08:23 +0900

    divider css 수정

--- packages/svelte-notion-page/src/lib/default-notion-components/Divider.svelte
+++ packages/svelte-notion-page/src/lib/default-notion-components/Divider.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 </script>
+
 <hr class="notion-block notion-hr" />
 
 <style>
 	.notion-hr {
 		margin: 6px 0;
 		padding: 0;
-		border-top: none;
-		border-color: var(--fg-color-1);
+		border: none;
+		border-top: 1px solid var(--fg-color-1);
 	}
 </style>
