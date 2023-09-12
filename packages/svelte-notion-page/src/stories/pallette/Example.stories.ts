30b9b762936550bb051ba7e266e0e167c501a7a2
daeseungmoon <tmdeoans@snu.ac.kr>
2023-09-12 09:23:29 +0900

    dark 모드 스토리 추가

--- packages/svelte-notion-page/src/stories/pallette/Example.stories.ts
+++ packages/svelte-notion-page/src/stories/pallette/Example.stories.ts
@@ -4,7 +4,10 @@ import Example from './Example.svelte';
 
 const meta = {
 	title: 'Example/Pallette',
-	component: Example as any
+	component: Example as any,
+	args: {
+		theme: 'light'
+	}
 } satisfies Meta<Example>;
 
 export default meta;
@@ -12,6 +15,17 @@ type Story = StoryObj<typeof meta>;
 
 export const Desktop: Story = {
 	args: {},
+	parameters: {
+		viewport: {
+			defaultViewport: 'desktop'
+		}
+	}
+};
+
+export const DarkTheme: Story = {
+	args: {
+		theme: 'dark'
+	},
 	parameters: {
 		viewport: {
 			defaultViewport: 'desktop'
